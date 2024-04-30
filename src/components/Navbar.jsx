224e902dc14a8d7bc7bf05d7fd164bcccc3a67b8
CheeseLad <cheesescreativeserver@gmail.com>
2024-04-30 03:05:57 +0100

    mobile nav start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo/logo.png';
 import JoinButton from './Joinbutton';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
       <div className="flex items-center">
@@ -10,7 +16,30 @@ function Navbar() {
         <img src={logo} alt="Logo" className="w-12 h-auto" />
         <span className="text-white text-lg font-semibold ml-2">DCU Fotosoc</span>
       </div>
-      <div className="flex space-x-12">
+      {/* Mobile hamburger menu */}
+      <div className="block md:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none focus:bg-gray-700 px-2 py-1 rounded-lg"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+      {/* Desktop navigation */}
+      <div className={`md:flex space-x-12 ${menuOpen ? 'block' : 'hidden'}`}>
         <a href="/" className="text-white text-2xl hover:text-gray-300">Home</a>
         <a href="/gallery" className="text-white text-2xl hover:text-gray-300">Gallery</a>
         <a href="/committee" className="text-white text-2xl hover:text-gray-300">Committee</a>
